6feda6c3f042211a8653ae5202f972fc08b280cd
8.32 <2636064351@qq.com>
2026-08-28 10:53:59 +0800

    feat: add shadcn and ui components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Toaster } from '@/components/ui/sonner';
+import { TooltipProvider } from '@/components/ui/tooltip';
 import { Navigation, AppRoutes } from './router/Routes';
 
 // 主应用包装器
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="app">
-        <Navigation />
-        <AppRoutes />
-      </div>
+      <TooltipProvider>
+        <div className="app">
+          <Navigation />
+          <AppRoutes />
+        </div>
+        <Toaster />
+      </TooltipProvider>
     </Router>
   );
 };
